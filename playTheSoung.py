ca57ab3bc7a5be15adcbc6d48dc0127c5312bb77
Jakeone Im <jakeoneijk@gmail.com>
2020-06-21 22:23:49 +0900

    check that my code work

--- playTheSoung.py
+++ playTheSoung.py
@@ -2,7 +2,7 @@ import pygame as pg
 import time
 
 #ssh pi@192.168.1.248
-
+'''
 from socket import *
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('192.168.1.248', 8080))
@@ -11,10 +11,7 @@ clientSock.send('I am a client'.encode('utf-8'))
 print('메시지를 전송했습니다.')
 data = clientSock.recv(1024)
 print('받은 데이터 : ', data.decode('utf-8'))
-
-while True:
-    print(clientSock.recv(1024))
-
+'''
 soundType = {'ORIGINAL':0 , 'REVERB':1 , 'DISTORTED':2 , 'REVERBandDISTORTED':3}
 motionType = {'verticalMotion':0 , 'horizontalMotion':1,'circleMotion':2}
 
@@ -22,7 +19,7 @@ distortedActivated = 0
 reverbActivated = 0
 
 soundDir = []
-soundDir.append(["./sound/dis_kic.wav"])    #original sound dir
+soundDir.append(["./sound/normal_kick.wav","./sound/normal_snare.wav","./sound/normal_purcu.wav","./sound/normal_synth.wav"])    #original sound dir
 soundDir.append([])                                     #reverb sound dir
 soundDir.append([])                                     #distorted sound dir
 soundDir.append([])                                     #distorted reverb sound dir
@@ -39,21 +36,28 @@ def initiate(): #store the sound
 
 def playTheSound(type , index):
     sound[type][index].play()
-    time.sleep(0.3)
+    time.sleep(0.01)
 
 def receiveLeap(): # return -1: no leapmotion , 1: leapmotion activated
     print("receiveLeapMotion")
     return -1
 
 def receiveAccSensor(): # return -1: no acc motion , 0 : verticalMotion , 1: horizontalMotion , 2: circleMotion
-    print("receiveAccSensor")
-    return 0
+    receive = clientSock.recv(1024)[0]
+    print("receiveAccSensor : ",receive)
+    return receive
 
 def receiveLightSensor(): # return -1: no lightmotion , 1: lightmotion activated
     print("receiveLightSensorSensor")
     return -1
 
 initiate()
+playTheSound(soundType['ORIGINAL'],0)
+playTheSound(soundType['ORIGINAL'],0)
+playTheSound(soundType['ORIGINAL'],0)
+playTheSound(soundType['ORIGINAL'],0)
+playTheSound(soundType['ORIGINAL'],0)
+time.sleep(100)
 
 while True:
     if receiveLeap() == 1:
