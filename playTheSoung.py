faed96530dc184861ec63e32905066a206cd8792
Jakeone Im <jakeoneijk@gmail.com>
2020-06-21 20:46:51 +0900

    modify ip

--- playTheSoung.py
+++ playTheSoung.py
@@ -5,7 +5,7 @@ import time
 
 from socket import *
 clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', 8080))
+clientSock.connect(('192.168.1.248', 8080))
 print('연결 확인 됐습니다.')
 clientSock.send('I am a client'.encode('utf-8'))
 print('메시지를 전송했습니다.')
