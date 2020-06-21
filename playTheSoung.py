43b8889713546455d0f49c47f4b6e26e68ae36e6
Jakeone Im <jakeoneijk@gmail.com>
2020-06-21 20:43:50 +0900

    socket

--- playTheSoung.py
+++ playTheSoung.py
@@ -3,6 +3,15 @@ import time
 
 #ssh pi@192.168.1.248
 
+from socket import *
+clientSock = socket(AF_INET, SOCK_STREAM)
+clientSock.connect(('127.0.0.1', 8080))
+print('연결 확인 됐습니다.')
+clientSock.send('I am a client'.encode('utf-8'))
+print('메시지를 전송했습니다.')
+data = clientSock.recv(1024)
+print('받은 데이터 : ', data.decode('utf-8'))
+
 soundType = {'ORIGINAL':0 , 'REVERB':1 , 'DISTORTED':2 , 'REVERBandDISTORTED':3}
 motionType = {'verticalMotion':0 , 'horizontalMotion':1,'circleMotion':2}
 
